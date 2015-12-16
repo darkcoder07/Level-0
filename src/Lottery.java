4f5f8473fd449fb103056819ad3bbfcc38629781
Mrfuzzy <league@WTS8.attlocal.net>
2015-12-15 16:59:58 -0800

    Happy Holidays!

---
+++ src/Lottery.java
@@ -0,0 +1,67 @@
+import java.util.Random;
+
+import javax.swing.JOptionPane;
+
+public class Lottery {
+
+public static void main(String[] args) {
+
+	
+	
+	
+	
+	
+	
+	
+	
+JOptionPane.showMessageDialog(null, "Lets give you your lottery numbers! (May or may not be a winning card.)");	
+for (int i=0; i<6; i++)	{
+int random = new Random().nextInt(48)+1;
+
+JOptionPane.showMessageDialog(null,  "Okay, one of your six lottery numbers are:" + random);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+}
