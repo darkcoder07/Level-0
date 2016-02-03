836816f31b472ce7b56ba93470b0261c32ac7fc1
PhazeIllusions <league@WTS14.attlocal.net>
2016-02-02 17:00:07 -0800

    Tests and partially attempted names

---
+++ src/Vishenator.java
@@ -0,0 +1,45 @@
+import org.teachingextensions.logo.Tortoise;
+
+public class Vishenator {
+
+public static void main(String[] args) {
+Tortoise.penUp();
+Tortoise.moveTo(50, 50);
+Tortoise.turn(155);	
+Tortoise.move(60);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
